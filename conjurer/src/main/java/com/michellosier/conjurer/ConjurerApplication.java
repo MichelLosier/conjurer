85e84adb94dcdb82047696e1f3dc19772ac3d334
Michel Losier <mikelosier@gmail.com>
2019-03-18 11:33:55 -0700

    first commit

---
+++ conjurer/src/main/java/com/michellosier/conjurer/ConjurerApplication.java
@@ -0,0 +1,13 @@
+package com.michellosier.conjurer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConjurerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConjurerApplication.class, args);
+	}
+
+}
